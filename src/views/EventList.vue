0f38fed08b2f1ccd5f2afd29a2a09933ab116668
mira1234 <miracupkic@gmail.com>
2020-04-19 13:27:37 +0200

    changing basic components and establishing routes for new components

---
+++ src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>Events Listing</h1>
+        <router-link :to="{name : 'event-show', params:{id:'1'}}" >Show event</router-link> |
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="user">
+      <h1>This is {{username}} state of a curent route User</h1>
+      <p>$route.params.username - {{$route.params.username}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["username"]
+
+}
+</script>
+
+<style>
+
+</style>
